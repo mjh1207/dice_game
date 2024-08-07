ecaae3135fdf97ca6323dbeb9485b2bb5b184cd4
mjh1207 <mitchh1207@gmail.com>
2024-08-07 14:55:13 -0600

    Organize GameWindow class a bit

--- src/graphics.py
+++ src/graphics.py
@@ -3,11 +3,14 @@ from tkinter import BOTH, Tk, Canvas, Button, Label, Frame
 class GameWindow:
     def __init__(self, width=800, height=800):
         self._primary_color = "#f0e399"
+        
         # Game window
         self.root = Tk()
         self.root.geometry(f"{width}x{height}")
         self.root.title("Dice!")
         self.root.configure(bg="#f0e399")
+        self.running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
 
         # Canvas and header
         self.canvas = Canvas(self.root, bg=self._primary_color, width=width, height=height)
@@ -20,15 +23,10 @@ class GameWindow:
         self.footer_frame.pack(side="bottom")
         self.help_button = Button(self.footer_frame, text="How to play",font=("Arial", 16), bg="white",)
         self.new_game_button = Button(self.footer_frame, text="New Game", font=("Arial", 16), bg="white")
-
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
-
         self.help_button.grid(row=0, column=0, padx=5, pady=10)
         self.new_game_button.grid(row=0, column=1, padx=5, pady=10)
-        self.running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
-
 
     def redraw(self):
         self.root.update_idletasks()
